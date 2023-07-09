85d9f02b504a55ad99ae596af2c5266637c5ca45
22051677-Wang zhen <1240306894@qq.com>
2023-07-09 15:24:32 +0800

    单步执行与跟踪函数

--- PA0/Learn_C/Learn/chapter1/10_gdb/1_backtrac_frame/e10-1.c
+++ PA0/Learn_C/Learn/chapter1/10_gdb/1_backtrac_frame/e10-1.c
@@ -1,8 +1,8 @@
 /*************************************************************************
-	> File Name: example.c
+	> File Name: e10-1.c
 	> Author :WangZhen
 	> Mail: 1240306894@qq.com
-	> Created Time: 2023年07月01日 星期六 21时31分41秒
+	> Created Time: 2023年07月08日 星期六 20时21分32秒
  ************************************************************************/
 
 #include<stdio.h>
@@ -10,20 +10,16 @@
 int add_range(int low, int high)
 {
 	int i, sum;
-	sum = 0;
-	i = 0;
-	for (i = low; i <= high; i++)
+	for(i = low; i <= high; i++)
 		sum = sum + i;
-	
 	return sum;
 }
 
-
 int main(int argc, char *argv[])
 {
-		int result[100];
-		result[0] = add_range(1, 10);
-	    result[1] = add_range(1, 100);
-		printf("result[0]=%d\nresult[1]=%d\n", result[0], result[1]);
-		return 0;
+	int result[100];
+	result[0] = add_range(1, 10);
+	result[1] = add_range(1, 100);
+	printf("result[0]=%d\nresult[1]=%d\n", result[0], result[1]);
+	return 0;
 }
