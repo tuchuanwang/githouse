500d4e9991643354703a5ba6fbfcdc1c79ab3cb4
22051677-Wang zhen <1240306894@qq.com>
2023-07-18 20:35:51 +0800

    界面显示

--- ysyx_bilibili/time.sh
+++ ysyx_bilibili/time.sh
@@ -8,6 +8,6 @@
 PATH=/home/edison/bin:/home/edison/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/work/tools/gcc-3.4.5-glibc-2.3.6/bin
 export PATH
 
-watch -t -n 1 "echo -n '一生一芯 | 预学习P线 | '; date;"
-
-bash ./cpu.sh
+watch -t -n 1 "echo -n '一生一芯 | 预学习P线 | '; date; cat /proc/cpuinfo | grep MHz | awk '{print \$1 NR \$3 \$4 \$2}'"
+#watch -n 1 "cat /proc/cpuinfo | grep MHz | awk '{print \$1 NR \$3 \$4 \$2}' "
+#bash ./cpu.sh
